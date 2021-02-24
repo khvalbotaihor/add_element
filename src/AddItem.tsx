7e17c9ed247f1a752a4694a5ef8986a9edda639c
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-02-24 16:44:27 +0200

    Initial commit

---
+++ src/AddItem.tsx
@@ -0,0 +1,45 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+
+type AddItemPropsType = {
+    title: string
+    id: string
+    addTask: (title: string, id: string) => void
+}
+export const AddItem = (props: AddItemPropsType) => {
+
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const addTask = () => {
+        let newTitle = title.trim();
+        if (newTitle !== "") {
+            props.addTask(newTitle, props.id);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.charCode === 13) {
+            addTask();
+        }
+    }
+
+    return (
+        <div>
+            <input value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   className={error ? "error" : ""}
+            />
+            <button onClick={addTask}>+</button>
+            {error && <div className="error-message">{error}</div>}
+        </div>
+    )
+}
